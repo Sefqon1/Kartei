11a8963b18f0c5d45d31aaf3770a4394f46f7645
Sefqon1 <severin.klee@gmail.com>
2022-09-05 20:55:33 +0700

    initial commit

---
+++ Adresse.java
@@ -0,0 +1,41 @@
+public class Adresse {
+
+    public int plz = 0;
+    public String ort;
+    public String strasse;
+
+    //Konstruktor fuer Fall, dass keine Parameter
+
+    public Adresse(int plz, String ort, String strasse) {
+        this.plz = plz;
+        this.ort = ort;
+        this.strasse = strasse;
+    }
+
+    public int get_plz() {
+        return plz;
+    }
+
+    public void set_plz(int new_plz) {
+        this.plz = new_plz;
+    }
+
+    public String get_ort() {
+        return ort;
+    }
+
+    public void set_ort(String new_ort) {
+        this.ort = new_ort;
+    }
+
+    public String get_strasse() {
+        return strasse;
+    }
+
+    public void set_strasse(String new_strasse) {
+        this.strasse = new_strasse;
+    }
+
+
+
+}
---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    static int void main(String[] args) {
+        Freund freund1 = new Freund('Severin', 'Kleesattel', '30.04.1996', Adresse sAdresse = new Adresse(71522, 'Backnang', 'Weinberg'));
+        Kartei kartei1 = new Kartei();
+        freund1.del_freund(this.nachname, this.schlussel);
+        //aenderung
+
+        // for (entry in array) {
+        // print array[i] }
+    }
+
+}
